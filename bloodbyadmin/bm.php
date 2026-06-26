4f5425ce8f2fb8ec3aa9ab7447ad3b64bb501da0
AhmedAL-Sorori <e.ahmedal.sorori@gmail.com>
2026-06-26 06:33:29 +0300

    Add Types Bloods
    فصائل الدم الرئيسية

---
+++ bloodbyadmin/bm.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+include("../../auth/db_connect.php");
+// $query="select count(*) from donors where blood_group =".'4';
+$query="select count(*) from donation dn , donors d, doctor dc , laboratory l 
+where dn.d_id=d.d_id
+ and dn.do_id=dc.do_id 
+ and dc.l_id=l.l_id 
+ 
+ and d.blood_group=".'4';
+// echo $query;exit();
+$result=mysqli_query($conn,$query);
+$row=mysqli_fetch_array($result);
+$_SESSION['bm']= $row['0'];
+mysqli_close($conn);
+?>
+</body>
+</html>
---
+++ bloodbyadmin/op.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+// session_start();
+include("../../auth/db_connect.php");
+// $query="select count(*) from donation dn ,donors do, doctor dc , laboratory l, where dn. blood_group =".'5';
+$query="select count(*) from donation dn , donors d, doctor dc , laboratory l 
+where dn.d_id=d.d_id
+ and dn.do_id=dc.do_id 
+ and dc.l_id=l.l_id 
+ 
+ and d.blood_group=".'5';
+// echo $query;exit();
+$result=mysqli_query($conn,$query);
+$row=mysqli_fetch_array($result);
+$_SESSION['op']= $row['0'];
+mysqli_close($conn);
+?>
+</body>
+</html>
